775caa46e745dcb49ce286f4c6dcd20ae9f75cdb
Weiyüen Wu <doomsplayer@gmail.com>
2018-07-29 23:25:33 -0700

    do not unwrap in websocket

--- src/websockets.rs
+++ src/websockets.rs
@@ -1,4 +1,4 @@
-use errors::*;
+use errors::{BinanceError, Result};
 use model::*;
 use serde_json::from_str;
 use url::Url;
@@ -100,55 +100,51 @@ impl WebSockets {
         self.kline_handler = Some(Box::new(handler));
     }
 
-    pub fn event_loop(&mut self) {
-        loop {
-            if let Some(ref mut socket) = self.socket {
-                let msg: String = socket.0.read_message().unwrap().into_text().unwrap();
+    pub fn event_loop(&mut self) -> Result<()> {
+        while let Some(ref mut socket) = self.socket {
+            let msg: String = socket.0.read_message()?.into_text()?;
 
-                if msg.find(OUTBOUND_ACCOUNT_INFO) != None {
-                    let account_update: AccountUpdateEvent = from_str(msg.as_str()).unwrap();
+            if msg.find(OUTBOUND_ACCOUNT_INFO) != None {
+                let account_update: AccountUpdateEvent = from_str(&msg)?;
 
-                    if let Some(ref h) = self.user_stream_handler {
-                        h.account_update_handler(&account_update);
-                    }
-                } else if msg.find(EXECUTION_REPORT) != None {
-                    let order_trade: OrderTradeEvent = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.user_stream_handler {
-                        h.order_trade_handler(&order_trade);
-                    }
-                } else if msg.find(AGGREGATED_TRADE) != None {
-                    let trades: TradesEvent = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.market_handler {
-                        h.aggregated_trades_handler(&trades);
-                    }
-                } else if msg.find(DAYTICKER) != None {
-                    let trades: Vec<DayTickerEvent> = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.ticker_handler {
-                        h.day_ticker_handler(&trades);
-                    }
-                } else if msg.find(KLINE) != None {
-                    let kline: KlineEvent = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.kline_handler {
-                        h.kline_handler(&kline);
-                    }
-                } else if msg.find(PARTIAL_ORDERBOOK) != None {
-                    let partial_orderbook: OrderBook = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.market_handler {
-                        h.partial_orderbook_handler(&partial_orderbook);
-                    }
-                } else if msg.find(DEPTH_ORDERBOOK) != None {
-                    let depth_orderbook: DepthOrderBookEvent = from_str(msg.as_str()).unwrap();
-
-                    if let Some(ref h) = self.market_handler {
-                        h.depth_orderbook_handler(&depth_orderbook);
-                    }
+                if let Some(ref h) = self.user_stream_handler {
+                    h.account_update_handler(&account_update);
                 }
+            } else if msg.find(EXECUTION_REPORT) != None {
+                let order_trade: OrderTradeEvent = from_str(&msg)?;
+
+                if let Some(ref h) = self.user_stream_handler {
+                    h.order_trade_handler(&order_trade);
+                }
+            } else if msg.find(AGGREGATED_TRADE) != None {
+                let trades: TradesEvent = from_str(&msg)?;
+
+                if let Some(ref h) = self.market_handler {
+                    h.aggregated_trades_handler(&trades);
+                }
+            } else if msg.find(DAYTICKER) != None {
+                let trades: Vec<DayTickerEvent> = from_str(&msg)?;
+
+                if let Some(ref h) = self.ticker_handler {
+                    h.day_ticker_handler(&trades);
+                }
+            } else if msg.find(KLINE) != None {
+                let kline: KlineEvent = from_str(&msg)?;
+
+                if let Some(ref h) = self.kline_handler {
+                    h.kline_handler(&kline);
+                }
+            } else if msg.find(PARTIAL_ORDERBOOK) != None {
+                let partial_orderbook: OrderBook = from_str(&msg)?;
+
+                if let Some(ref h) = self.market_handler {
+                    h.partial_orderbook_handler(&partial_orderbook);
+                }
+            } else if msg.find(DEPTH_ORDERBOOK) != None {
+                let depth_orderbook: DepthOrderBookEvent = from_str(&msg)?;
+                self.market_handler.as_ref().map(|h| h.depth_orderbook_handler(&depth_orderbook));
             }
         }
+        Ok(())
     }
 }
